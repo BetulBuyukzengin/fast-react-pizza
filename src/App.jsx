d592be2d07c5b8722a73fb0cbfa519ea857c4bfc
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-03-09 18:25:01 +0300

    feat: use route error used

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,22 @@ import Cart from "./features/cart/Cart";
 import Order from "./features/order/Order";
 import CreateOrder from "./features/order/CreateOrder";
 import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
+
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       { path: "/", element: <Home /> },
       //! Step 2 in route loader : Provided loader functionality to a route
-      { path: "/menu", element: <Menu />, loader: menuLoader },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        //! I gave an errorElement here so that it does not affect the entire page.
+        errorElement: <Error />,
+      },
       { path: "/cart", element: <Cart /> },
       { path: "/order/new", element: <CreateOrder /> },
       { path: "/order:orderId", element: <Order /> },
