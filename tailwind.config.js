17cc9f04a0999b5a54a0974309e7c6449a66de39
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-03-11 23:10:26 +0300

    feat: add custom font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  //
   theme: {
-    extend: {},
+    // create new font fam
+    fontFamily: {
+      sans: 'Roboto Mono,monospace',
+    },
+    extend: {
+      // colors:{pizza:#32323}
+      height: {
+        screen: '100dvh',
+      },
+    },
   },
   plugins: [],
 };
