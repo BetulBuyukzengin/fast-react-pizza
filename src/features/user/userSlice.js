3ee03e2437a9ffb3070eb0833a751d4e6d69b2ec
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-03-14 17:40:24 +0300

    feat: integrate geolocation

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -49,10 +49,14 @@ const userSlice = createSlice({
         state.status = 'loading';
       })
       .addCase(fetchAddress.fulfilled, (state, action) => {
+        state.position = action.payload.position;
+        state.address = action.payload.address;
         state.status = 'idle';
       })
       .addCase(fetchAddress.rejected, (state, action) => {
-        (state.status = 'error'), (state.error = action.error.message);
+        (state.status = 'error'),
+          (state.error =
+            'There was a problem getting your address. Make sure to fill this field! ');
       }),
 });
 export const { updateName } = userSlice.actions;
